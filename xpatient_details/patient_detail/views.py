50b720a673651f4c9f1c3555019baee17a73fb40
Naveen-kumaar <navinsanjay2001@gmail.com>
2024-07-16 10:03:54 +0530

    code updated

--- xpatient_details/patient_detail/views.py
+++ xpatient_details/patient_detail/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
-from patient_detail.models import Patient
-from patient_detail.forms import Patientform
-from patient_detail.forms import Loginform
+# from patient_detail.models import Patient
+# from patient_detail.forms import Patientform
+# from patient_detail.forms import Loginform
 
 # # Create your views here.
 # def home(request):
@@ -15,25 +15,25 @@ from patient_detail.forms import Loginform
     #     Acpwd=request.POST.get('Conform_password')
     # return render(request,'register.html')
 
-def select_view(request):
-    patient1 = Patient.objects.all()
-    patient2 = {'patient':patient1}
+# def select_view(request):
+#     patient1 = Patient.objects.all()
+#     patient2 = {'patient':patient1}
 
-    return render(request,"base.html",context=patient2)
+#     return render(request,"base.html",context=patient2)
 
-def create_view(request):
-    form = Patientform()
-    if request.method =='POST':
-        form =Patientform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/check')
-        return render(request,'home.html',{'form':form})
-def login(request):
-    form = Loginform()
-    if request.method =='POST':
-        form = Loginform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/check')
-        return render(request,'login.html',{'form':form})
+# def create_view(request):
+#     form = Patientform()
+#     if request.method =='POST':
+#         form =Patientform(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/check')
+#         return render(request,'home.html',{'form':form})
+# def login(request):
+#     form = Loginform()
+#     if request.method =='POST':
+#         form = Loginform(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/check')
+#         return render(request,'login.html',{'form':form})
